b8897beb3f2b68ee1f00e5ef76e66e202b424e18
snowcrab382 <snowcrab382@gmail.com>
2024-07-16 19:12:35 +0900

    comment: 각 메서드 주석 작성

--- backend/src/main/java/perf/shop/global/error/ErrorResponse.java
+++ backend/src/main/java/perf/shop/global/error/ErrorResponse.java
@@ -1,10 +1,12 @@
 package perf.shop.global.error;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.validation.BindingResult;
+import perf.shop.global.error.exception.ErrorCode;
 
 @Getter
 @Builder
@@ -14,6 +16,27 @@ public class ErrorResponse {
     private String message;
     private List<FieldError> errors;
 
+    /**
+     * 일반적인 예외 처리 error에 대한 내용이 없는 경우 사용. errors 필드에는 null이 아닌 빈 리스트를 반환
+     *
+     * @param errorCode
+     * @return ErrorResponse
+     */
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+                .status(errorCode.getStatus())
+                .message(errorCode.getMessage())
+                .errors(Collections.emptyList())
+                .build();
+    }
+
+    /**
+     * MethodArgumentNotValidException 예외 처리
+     *
+     * @param errorCode
+     * @param errors
+     * @return ErrorResponse
+     */
     public static ErrorResponse of(ErrorCode errorCode, List<FieldError> errors) {
         return ErrorResponse.builder()
                 .status(errorCode.getStatus())
@@ -30,6 +53,12 @@ public class ErrorResponse {
         private Object value;
         private String message;
 
+        /**
+         * BindingResult의 FieldError 정보를 커스텀 FieldError로 변환
+         *
+         * @param bindingResult MethodArgumentNotValidException에서 전달받은 BindingResult
+         * @return List<FieldError>
+         */
         public static List<FieldError> of(BindingResult bindingResult) {
             return bindingResult.getFieldErrors()
                     .stream()

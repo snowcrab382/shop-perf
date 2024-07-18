1fc056162ac5b9efc2a85636323d2392a15f81f2
snowcrab382 <snowcrab382@naver.com>
2024-07-18 19:33:32 +0900

    feat: jwt필터, 인증성공핸들러 설정 추가

--- backend/src/main/java/perf/shop/global/config/security/SecurityConfig.java
+++ backend/src/main/java/perf/shop/global/config/security/SecurityConfig.java
@@ -1,2 +1,54 @@
-package perf.shop.global.config.security;public class SecurityConfig {
+package perf.shop.global.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import perf.shop.domain.auth.handler.OAuth2LoginSuccessHandler;
+import perf.shop.domain.auth.jwt.JwtFilter;
+import perf.shop.domain.auth.jwt.JwtUtil;
+import perf.shop.domain.user.service.CustomOAuth2UserService;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2LoginSuccessHandler customSuccessHandler;
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                //csrf disable
+                .csrf(AbstractHttpConfigurer::disable)
+                //From 로그인 방식 disable
+                .formLogin(AbstractHttpConfigurer::disable)
+                //HTTP Basic 인증 방식 disable
+                .httpBasic(AbstractHttpConfigurer::disable)
+                //jwt 검증 필터
+                .addFilterAfter(new JwtFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class)
+                //oauth2
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler))
+                //경로별 인가 작업
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                        .anyRequest().authenticated())
+                //세션 설정 : STATELESS
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 }

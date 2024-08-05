cfe213afffe6ec2635a9ea93887b34201ae1db0f
snowcrab382 <snowcrab382@naver.com>
2024-08-05 17:20:33 +0900

    chore: category entity 생성

--- backend/src/main/java/perf/shop/domain/product/domain/Category.java
+++ backend/src/main/java/perf/shop/domain/product/domain/Category.java
@@ -1,2 +1,26 @@
-package perf.shop.domain.product.domain;public class Category {
+package perf.shop.domain.product.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import perf.shop.global.common.domain.BaseEntity;
+
+@Entity
+@Getter
+@Table(name = "category")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
 }

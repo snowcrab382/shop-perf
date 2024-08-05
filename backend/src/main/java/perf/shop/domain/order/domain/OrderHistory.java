a898455625b7ae186a5c160f6c98c940d674f95e
snowcrab382 <snowcrab382@naver.com>
2024-08-05 17:42:23 +0900

    chore: orderHistory entity 생성

---
+++ backend/src/main/java/perf/shop/domain/order/domain/OrderHistory.java
@@ -0,0 +1,32 @@
+package perf.shop.domain.order.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import perf.shop.global.common.domain.BaseEntity;
+
+@Entity
+@Getter
+@Table(name = "order_history")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Column(length = 20, nullable = false)
+    private String status;
+}

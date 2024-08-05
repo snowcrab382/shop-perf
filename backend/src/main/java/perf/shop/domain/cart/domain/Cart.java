4ab2212296c04711e22e4907386ef633daa355e9
snowcrab382 <snowcrab382@naver.com>
2024-08-05 17:25:05 +0900

    chore: cart entity 생성

--- backend/src/main/java/perf/shop/domain/cart/domain/Cart.java
+++ backend/src/main/java/perf/shop/domain/cart/domain/Cart.java
@@ -1,2 +1,30 @@
-package perf.shop.domain.cart.domain;public class Cart {
+package perf.shop.domain.cart.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import perf.shop.domain.user.domain.User;
+import perf.shop.global.common.domain.BaseEntity;
+
+@Entity
+@Getter
+@Table(name = "cart")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cart extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }

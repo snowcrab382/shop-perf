b072abe5c3b8cf554231055f7a2c82c87b91eb06
snowcrab382 <snowcrab382@naver.com>
2024-08-05 17:19:00 +0900

    chore: product entity 생성

--- backend/src/main/java/perf/shop/domain/product/domain/Product.java
+++ backend/src/main/java/perf/shop/domain/product/domain/Product.java
@@ -1,12 +1,49 @@
 package perf.shop.domain.product.domain;
 
-import lombok.Builder;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import perf.shop.domain.user.domain.User;
+import perf.shop.global.common.domain.BaseEntity;
 
-@Builder
+@Entity
 @Getter
-public class Product {
+@Table(name = "product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product extends BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
+
+    @Column(length = 30, nullable = false)
     private String name;
+
+    private String image;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(nullable = false)
+    private Long stock;
 }

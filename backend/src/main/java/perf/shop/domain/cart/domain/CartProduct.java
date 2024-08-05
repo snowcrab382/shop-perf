7782f0ddad1f9da88f13e586b5e8ea7288cb7d26
snowcrab382 <snowcrab382@naver.com>
2024-08-05 17:22:27 +0900

    chore: cartProduct entity 생성

--- backend/src/main/java/perf/shop/domain/cart/domain/CartProduct.java
+++ backend/src/main/java/perf/shop/domain/cart/domain/CartProduct.java
@@ -1,2 +1,41 @@
-package perf.shop.domain.cart.domain;public class CartProduct {
+package perf.shop.domain.cart.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import perf.shop.domain.product.domain.Product;
+import perf.shop.global.common.domain.BaseEntity;
+
+@Entity
+@Getter
+@Table(name = "cart_product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartProduct extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Long price;
 }

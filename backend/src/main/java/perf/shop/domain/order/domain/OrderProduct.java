20a2eecd42a004668cfd47af7a6bde87c65b5c53
snowcrab382 <snowcrab382@naver.com>
2024-08-05 17:21:25 +0900

    chore: orderProduct entity 생성

--- backend/src/main/java/perf/shop/domain/order/domain/OrderProduct.java
+++ backend/src/main/java/perf/shop/domain/order/domain/OrderProduct.java
@@ -1,2 +1,41 @@
-package perf.shop.domain.order.domain;public class OrderProduct {
+package perf.shop.domain.order.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import perf.shop.domain.product.domain.Product;
+import perf.shop.global.common.domain.BaseEntity;
+
+@Entity
+@Getter
+@Table(name = "order_product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderProduct extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Long price;
+
 }

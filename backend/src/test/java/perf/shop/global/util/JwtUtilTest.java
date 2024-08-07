76243897ea246117f5df2f376a7e0a6ebb208e72
snowcrab382 <snowcrab382@naver.com>
2024-08-07 16:35:53 +0900

    test: JwtUtil 테스트코드 작성 - 테스트 시점에 JwtUtil의 secretKey에 사전에 정의한 값을 ReflectionUtils를 이용하여 설정하여 사용 - Util의 가장 중요한 로직인 유효성 검증 메서드의 실패케이스 테스트 작성

---
+++ backend/src/test/java/perf/shop/global/util/JwtUtilTest.java
@@ -0,0 +1,73 @@
+package perf.shop.global.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@DisplayName("[단위 테스트] JwtUtil")
+class JwtUtilTest {
+
+    @BeforeEach
+    void setUp() {
+        String secretKey = "testsecretkeytestsecretkeytestsecretkeytestsecretkeytestsecretkeytestsecretkey";
+        ReflectionTestUtils.setField(JwtUtil.class, "secretKey",
+                Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    @Nested
+    @DisplayName("jwt 유효성 검사 테스트")
+    class IsValid {
+
+        @Test
+        @DisplayName("jwt가 변조되었다면 예외 발생")
+        void FailToValidJwtIfMalformed() {
+            //given
+            String jwt = JwtUtil.createJwt("user", "ROLE_USER", 1000L);
+            String malformedJwt = "a" + jwt;
+
+            //when
+
+            //then
+            assertThatThrownBy(() -> JwtUtil.isValid(malformedJwt))
+                    .isInstanceOf(MalformedJwtException.class);
+        }
+
+        @Test
+        @DisplayName("jwt의 시그니쳐가 변경되었다면 예외 발생")
+        void FailToValidJwtIfSignatureIsDifferent() {
+            //given
+            String jwt = JwtUtil.createJwt("user", "ROLE_USER", 1000L);
+            String differentSignatureJwt = jwt + "a";
+
+            //when
+
+            //then
+            assertThatThrownBy(() -> JwtUtil.isValid(differentSignatureJwt))
+                    .isInstanceOf(SignatureException.class);
+        }
+
+        @Test
+        @DisplayName("jwt가 만료되었다면 예외 발생")
+        void FailToValidJwtIfExpired() {
+            //given
+            String expiredJwt = JwtUtil.createJwt("user", "ROLE_USER", 0L);
+
+            //when
+
+            //then
+            assertThatThrownBy(() -> JwtUtil.isValid(expiredJwt))
+                    .isInstanceOf(ExpiredJwtException.class);
+        }
+
+    }
+
+}

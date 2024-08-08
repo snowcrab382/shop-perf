85025330ec51f090abd4156ef81d4d8b9babd421
snowcrab382 <snowcrab382@naver.com>
2024-08-08 16:28:55 +0900

    refactor: 테스트용 api 삭제

--- backend/src/main/java/perf/shop/domain/product/api/ProductController.java
+++ backend/src/main/java/perf/shop/domain/product/api/ProductController.java
@@ -1,22 +1,9 @@
-package perf.shop.domain.product.controller;
+package perf.shop.domain.product.api;
 
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import perf.shop.domain.product.dto.RequestDto;
-import perf.shop.domain.product.dto.ResponseDto;
-import perf.shop.global.common.response.ApiResponse;
-import perf.shop.global.common.response.ResponseCode;
 
 @RestController
 public class ProductController {
 
-    @GetMapping("/test")
-    public ApiResponse<ResponseDto> test(@Valid RequestDto request) {
-        ResponseDto responseDto = new ResponseDto();
-        responseDto.setId(request.getId());
-        responseDto.setName(request.getName());
-        return ApiResponse.of(ResponseCode.GET, responseDto);
-    }
 
 }

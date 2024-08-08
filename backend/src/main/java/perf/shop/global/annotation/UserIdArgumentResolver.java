ebf3e21155aa023d3dd7a3418ead77485e8dc644
snowcrab382 <snowcrab382@naver.com>
2024-08-08 23:08:27 +0900

    feat: 커스텀 애노테이션을 생성하여 인증 정보의 userId 컨트롤러로 전달

---
+++ backend/src/main/java/perf/shop/global/annotation/UserIdArgumentResolver.java
@@ -0,0 +1,29 @@
+package perf.shop.global.annotation;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import perf.shop.domain.user.dto.CustomOAuth2User;
+
+public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(UserId.class) != null;
+    }
+
+    @Override
+    public Long resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof CustomOAuth2User userDetails) {
+            return userDetails.getUserId();
+        }
+        throw new AccessDeniedException("");
+    }
+}

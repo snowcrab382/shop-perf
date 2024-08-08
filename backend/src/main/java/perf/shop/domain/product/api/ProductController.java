0b75649bc28905e4233da484e83e588c9141ab12
snowcrab382 <snowcrab382@naver.com>
2024-08-08 16:28:39 +0900

    refactor: controller -> api로 패키지명 변경

--- backend/src/main/java/perf/shop/domain/product/api/ProductController.java
+++ backend/src/main/java/perf/shop/domain/product/api/ProductController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import perf.shop.domain.product.dto.RequestDto;
 import perf.shop.domain.product.dto.ResponseDto;
-import perf.shop.domain.product.exception.ProductNotFoundException;
 import perf.shop.global.common.response.ApiResponse;
 import perf.shop.global.common.response.ResponseCode;
 
@@ -18,21 +17,6 @@ public class ProductController {
         responseDto.setId(request.getId());
         responseDto.setName(request.getName());
         return ApiResponse.of(ResponseCode.GET, responseDto);
-    }
-
-    @GetMapping("/test2")
-    public ApiResponse<String> test2(@Valid RequestDto request) {
-        return ApiResponse.of(ResponseCode.GET, "test");
-    }
-
-    @GetMapping("/test3")
-    public ApiResponse<Void> test3(@Valid RequestDto request) {
-        return ApiResponse.of(ResponseCode.GET);
-    }
-
-    @GetMapping("/test4")
-    public ApiResponse<Void> test4() {
-        throw new ProductNotFoundException();
     }
 
 }

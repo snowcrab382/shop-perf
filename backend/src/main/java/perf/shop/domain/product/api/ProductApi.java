d649c4bb17c79bbc641396f47493edb81a951e91
snowcrab382 <snowcrab382@naver.com>
2024-08-09 12:21:06 +0900

    feat: productApi 생성 - product 생성 시 CREATED 응답

---
+++ backend/src/main/java/perf/shop/domain/product/api/ProductApi.java
@@ -0,0 +1,27 @@
+package perf.shop.domain.product.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import perf.shop.domain.product.application.ProductService;
+import perf.shop.domain.product.dto.request.ProductSaveRequest;
+import perf.shop.global.annotation.UserId;
+import perf.shop.global.common.response.ApiResponse;
+import perf.shop.global.common.response.ResponseCode;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductApi {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ApiResponse<Void> save(@RequestBody ProductSaveRequest productSaveRequest,
+                                  @UserId Long sellerId) {
+        productService.saveProduct(productSaveRequest, sellerId);
+        return ApiResponse.of(ResponseCode.CREATED);
+    }
+}

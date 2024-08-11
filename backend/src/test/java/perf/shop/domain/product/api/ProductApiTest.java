4992eaeab3ad0043a4242a6e92303d8a6e2c0d2a
snowcrab382 <snowcrab382@naver.com>
2024-08-11 17:00:18 +0900

    test: 상품 단건 조회 단위테스트 작성

--- backend/src/test/java/perf/shop/domain/product/api/ProductApiTest.java
+++ backend/src/test/java/perf/shop/domain/product/api/ProductApiTest.java
@@ -1,12 +1,14 @@
 package perf.shop.domain.product.api;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -19,7 +21,10 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import perf.shop.domain.product.application.ProductService;
 import perf.shop.domain.product.dto.request.ProductSaveRequest;
+import perf.shop.domain.product.dto.response.ProductFindByIdResponse;
+import perf.shop.domain.product.exception.ProductNotFoundException;
 import perf.shop.global.common.response.ResponseCode;
+import perf.shop.global.error.exception.ErrorCode;
 import perf.shop.mock.InjectMockUser;
 
 @InjectMockUser
@@ -72,5 +77,64 @@ class ProductApiTest {
                     .andExpect(jsonPath("$.data", equalTo(null)));
         }
 
+    }
+
+    @Nested
+    @DisplayName("상품 조회 API 테스트")
+    class FindById {
+
+        @Test
+        @DisplayName("상품 조회 성공")
+        void findById_Success() throws Exception {
+            // given
+            Long id = 1L;
+            ProductFindByIdResponse productDetail = ProductFindByIdResponse.builder()
+                    .name("상품명")
+                    .price(10000L)
+                    .image("상품 이미지 URL")
+                    .description("상품 설명")
+                    .stock(100L)
+                    .build();
+            given(productService.findProductById(id)).willReturn(productDetail);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/products/{id}", id)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print());
+
+            // then
+            resultActions
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status", equalTo(ResponseCode.GET.getStatus())))
+                    .andExpect(jsonPath("$.message", equalTo(ResponseCode.GET.getMessage())))
+                    .andExpectAll(
+                            jsonPath("$.data.name").exists(),
+                            jsonPath("$.data.price").exists(),
+                            jsonPath("$.data.image").exists(),
+                            jsonPath("$.data.description").exists(),
+                            jsonPath("$.data.stock").exists()
+                    );
+        }
+
+        @Test
+        @DisplayName("상품이 존재하지 않으면 예외 발생")
+        void findById_ThrowException_IfProductNotFound() throws Exception {
+            // given
+            Long id = 1L;
+            given(productService.findProductById(id)).willThrow(
+                    new ProductNotFoundException(ErrorCode.PRODUCT_NOT_FOUND));
+
+            // when
+            ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/products/{id}", id)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print());
+
+            // then
+            resultActions
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status", equalTo(ErrorCode.PRODUCT_NOT_FOUND.getStatus())))
+                    .andExpect(jsonPath("$.message", equalTo(ErrorCode.PRODUCT_NOT_FOUND.getMessage())))
+                    .andExpect(jsonPath("$.errors", equalTo(List.of())));
+        }
     }
 }
--- backend/src/test/java/perf/shop/domain/product/application/ProductServiceTest.java
+++ backend/src/test/java/perf/shop/domain/product/application/ProductServiceTest.java
@@ -1,9 +1,12 @@
 package perf.shop.domain.product.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -14,6 +17,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import perf.shop.domain.product.dao.ProductRepository;
 import perf.shop.domain.product.domain.Product;
 import perf.shop.domain.product.dto.request.ProductSaveRequest;
+import perf.shop.domain.product.dto.response.ProductFindByIdResponse;
+import perf.shop.domain.product.exception.ProductNotFoundException;
+import perf.shop.global.error.exception.ErrorCode;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("[단위 테스트] ProductService")
@@ -48,6 +54,51 @@ class ProductServiceTest {
 
             // then
             then(productRepository).should().save(any(Product.class));
+
+        }
+    }
+
+    @Nested
+    @DisplayName("상품 조회 테스트")
+    class FindProductById {
+
+        @Test
+        @DisplayName("상품이 존재하면 조회 성공")
+        void findProductById_Success_IfProductExists() {
+            // given
+            Long productId = 1L;
+            Product product = Product.builder()
+                    .name("상품명")
+                    .price(10000L)
+                    .description("상품 설명")
+                    .stock(100L)
+                    .categoryId(1L)
+                    .build();
+            given(productRepository.findById(productId)).willReturn(Optional.ofNullable(product));
+
+            // when
+            ProductFindByIdResponse productFindById = productService.findProductById(productId);
+
+            // then
+            then(productRepository).should().findById(productId);
+            assertThat(productFindById).isInstanceOf(ProductFindByIdResponse.class);
+            assertThat(productFindById.getName()).isEqualTo(product.getName());
+            assertThat(productFindById.getPrice()).isEqualTo(product.getPrice());
+            assertThat(productFindById.getDescription()).isEqualTo(product.getDescription());
+            assertThat(productFindById.getStock()).isEqualTo(product.getStock());
+        }
+
+        @Test
+        @DisplayName("상품이 존재하지 않으면 예외 발생")
+        void findProductById_ThrowException_IfProductNotExists() {
+            // given
+            Long productId = 1L;
+            given(productRepository.findById(productId)).willReturn(Optional.empty());
+
+            // when & then
+            assertThatThrownBy(() -> productService.findProductById(productId))
+                    .isInstanceOf(ProductNotFoundException.class)
+                    .hasFieldOrPropertyWithValue("errorCode", ErrorCode.PRODUCT_NOT_FOUND);
 
         }
     }

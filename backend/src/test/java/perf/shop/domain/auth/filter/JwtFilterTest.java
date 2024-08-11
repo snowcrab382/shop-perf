d1d3aed15f2d87bda8eeaf5453fbbf12627b09d2
snowcrab382 <snowcrab382@naver.com>
2024-08-11 17:00:51 +0900

    test: 요청에 대한 JwtFilter 검증 로직 변경에 따른 테스트코드 수정

--- backend/src/test/java/perf/shop/domain/auth/filter/JwtFilterTest.java
+++ backend/src/test/java/perf/shop/domain/auth/filter/JwtFilterTest.java
@@ -10,12 +10,13 @@ import static perf.shop.domain.auth.domain.OAuth2Attributes.AUTHORIZATION;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import perf.shop.domain.auth.exception.JwtNotFoundException;
 import perf.shop.global.util.CookieUtil;
@@ -24,10 +25,14 @@ import perf.shop.global.util.CookieUtil;
 @DisplayName("[단위 테스트] JwtFilter")
 class JwtFilterTest {
 
+    @InjectMocks
     JwtFilter jwtFilter;
 
     @Mock
     HandlerExceptionResolver handlerExceptionResolver;
+
+    @Mock
+    RequestMatcher publicEndpoints;
 
     @Mock
     HttpServletRequest request;
@@ -38,17 +43,11 @@ class JwtFilterTest {
     @Mock
     FilterChain chain;
 
-    @BeforeEach
-    void setUp() {
-        String[] whitelist = new String[]{"/login"};
-        jwtFilter = new JwtFilter(handlerExceptionResolver, whitelist);
-    }
-
     @Test
     @DisplayName("화이트리스트에 포함된 uri인 경우 필터 로직을 수행하지 않고 바로 통과")
-    void doFilterInternal_Pass_IfRequestUriIsInWhitelist() throws Exception {
+    void doFilterInternal_Pass_IfRequestMatchesWithPublicEndpoints() throws Exception {
         //given
-        given(request.getRequestURI()).willReturn("/login");
+        given(publicEndpoints.matches(request)).willReturn(true);
 
         //when
         jwtFilter.doFilterInternal(request, response, chain);
@@ -59,9 +58,9 @@ class JwtFilterTest {
 
     @Test
     @DisplayName("쿠키가 비어있거나, Authorization이 존재하지 않으면 예외 발생")
-    void doFilterInternal_ThrowsException_IfCookieArrayOrAuthorizationCookieIsNull() throws Exception {
+    void doFilterInternal_ThrowException_IfCookieArrayOrAuthorizationCookieIsNull() throws Exception {
         //given
-        given(request.getRequestURI()).willReturn("/need-auth");
+        given(publicEndpoints.matches(request)).willReturn(false);
         given(CookieUtil.getCookie(request, AUTHORIZATION.getAttribute())).willReturn(null);
 
         //when

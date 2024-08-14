7a3fe6a1a1999a3adecd33737aee36f8c3410f3e
snowcrab382 <snowcrab382@naver.com>
2024-08-14 15:12:12 +0900

    refactor: 장바구니 서비스 로직 리팩토링 - 장바구니 목록조회 시 Empty 체크 로직 삭제 - 장바구니 단건 삭제 시 cartProduct를 곧바로 조회하도록 수정 - 단위테스트 수정

--- backend/src/main/java/perf/shop/domain/cart/repository/CartProductRepository.java
+++ backend/src/main/java/perf/shop/domain/cart/repository/CartProductRepository.java
@@ -1,9 +1,12 @@
 package perf.shop.domain.cart.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import perf.shop.domain.cart.domain.CartProduct;
 
 public interface CartProductRepository extends JpaRepository<CartProduct, Long> {
 
     void deleteAllByCartId(Long cartId);
+
+    Optional<CartProduct> findByIdAndCartId(Long id, Long cartId);
 }

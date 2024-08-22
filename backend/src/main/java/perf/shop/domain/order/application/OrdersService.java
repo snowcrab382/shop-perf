8cab94b02ae58b1f9b96a7a548c786dee8a6dad7
snowcrab382 <snowcrab382@naver.com>
2024-08-22 19:15:31 +0900

    feat: 주문 생성 로직 리팩토링 - 주문 생성 후 이벤트 발행 로직 삭제 - payment 엔티티는 결제 요청 시 생성하도록 변경

--- backend/src/main/java/perf/shop/domain/order/application/OrdersService.java
+++ backend/src/main/java/perf/shop/domain/order/application/OrdersService.java
@@ -2,17 +2,14 @@ package perf.shop.domain.order.application;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import perf.shop.domain.model.ShippingInfo;
-import perf.shop.domain.order.application.event.OrderCreateEvent;
 import perf.shop.domain.order.domain.Order;
 import perf.shop.domain.order.domain.OrderLine;
 import perf.shop.domain.order.domain.Orderer;
 import perf.shop.domain.order.dto.request.OrderCreateRequest;
 import perf.shop.domain.order.repository.OrdersRepository;
-import perf.shop.domain.payment.domain.PaymentInfo;
 
 @Transactional
 @Service
@@ -21,7 +18,6 @@ public class OrdersService {
 
     private final OrdersRepository ordersRepository;
     private final OrderLineFactory orderLineFactory;
-    private final ApplicationEventPublisher eventPublisher;
 
     public void createOrder(Long userId, OrderCreateRequest request) {
         Orderer orderer = Orderer.from(userId, request.getOrderer());
@@ -32,8 +28,5 @@ public class OrdersService {
 
         Order newOrder = Order.of(orderer, shippingInfo, orderLines);
         ordersRepository.save(newOrder);
-
-        PaymentInfo paymentInfo = PaymentInfo.from(request.getPaymentInfo());
-        eventPublisher.publishEvent(OrderCreateEvent.of(newOrder, paymentInfo));
     }
 }

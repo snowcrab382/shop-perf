55cbf44ba1661bd2393c323cd5a1942f0f6421a8
snowcrab382 <snowcrab382@naver.com>
2024-08-25 20:16:32 +0900

    fix: 테스트용 주문 조회 API 삭제

--- backend/src/main/java/perf/shop/domain/order/repository/OrdersRepository.java
+++ backend/src/main/java/perf/shop/domain/order/repository/OrdersRepository.java
@@ -1,12 +1,8 @@
 package perf.shop.domain.order.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import perf.shop.domain.order.domain.Order;
 
 public interface OrdersRepository extends JpaRepository<Order, String> {
 
-    @Query("SELECT o FROM Order o WHERE o.orderer.userId = :userId")
-    List<Order> findAllByUserId(Long userId);
 }

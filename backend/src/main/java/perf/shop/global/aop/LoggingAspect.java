08382c1ed206b56f77940dff12b926370b783c76
snowcrab382 <snowcrab382@naver.com>
2024-08-31 00:08:18 +0900

    refactor: 메서드 실행 중 예외 발생 시 로깅 추가 - 어떤 메서드 실행 중 어떤 예외가 발생했는지 추적하기 위해 로깅

--- backend/src/main/java/perf/shop/global/aop/LoggingAspect.java
+++ backend/src/main/java/perf/shop/global/aop/LoggingAspect.java
@@ -23,10 +23,18 @@ public class LoggingAspect {
         log.info("시작: {}.{}()", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
 
         // 실제 메서드 실행
-        Object result = joinPoint.proceed();
+        try {
+            Object result = joinPoint.proceed();
 
-        // 메서드 종료
-        log.info("종료: {}.{}()", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
-        return result;
+            // 메서드 종료
+            log.info("종료: {}.{}()", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName());
+            return result;
+        } catch (Exception e) {
+            // 메서드 예외
+            log.error("예외: {}.{}(), 클래스명 : {}", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), e.getClass());
+            throw e;
+        }
     }
 }

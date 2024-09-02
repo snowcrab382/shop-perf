aab78d2bf14cc395c8ad0571e234f3bdcb681808
snowcrab382 <snowcrab382@naver.com>
2024-09-02 19:59:52 +0900

    feat: 성능테스트 지표 비교를 위해 결제 메세지를 람다에서 처리하도록 변경

--- backend/src/main/java/perf/shop/infra/sqs/PaymentFailedSqsListener.java
+++ backend/src/main/java/perf/shop/infra/sqs/PaymentFailedSqsListener.java
@@ -1,6 +1,5 @@
 package perf.shop.infra.sqs;
 
-import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -17,12 +16,12 @@ public class PaymentFailedSqsListener {
     private final OrderService orderService;
     private final OutboxService outboxService;
 
-    @SqsListener(
-            value = "${spring.cloud.aws.sqs.payment-failed}",
-            factory = "defaultSqsListenerContainerFactory"
-    )
-    public void messageListener(PaymentFailedMessage response) {
-        orderService.cancelOrder(response.getOrderId());
-        outboxService.updateStatusToDone(response.getOrderId());
-    }
+//    @SqsListener(
+//            value = "${spring.cloud.aws.sqs.payment-failed}",
+//            factory = "defaultSqsListenerContainerFactory"
+//    )
+//    public void messageListener(PaymentFailedMessage response) {
+//        orderService.cancelOrder(response.getOrderId());
+//        outboxService.updateStatusToDone(response.getOrderId());
+//    }
 }
--- backend/src/main/java/perf/shop/infra/sqs/PaymentSuccessSqsListener.java
+++ backend/src/main/java/perf/shop/infra/sqs/PaymentSuccessSqsListener.java
@@ -1,7 +1,5 @@
 package perf.shop.infra.sqs;
 
-import io.awspring.cloud.sqs.annotation.SqsListener;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -9,7 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 import perf.shop.domain.order.application.OrderService;
 import perf.shop.domain.outbox.application.OutboxService;
 import perf.shop.domain.payment.application.PaymentService;
-import perf.shop.domain.payment.domain.Payment;
 
 @Slf4j
 @Transactional
@@ -21,17 +18,17 @@ public class PaymentSuccessSqsListener {
     private final OrderService orderService;
     private final OutboxService outboxService;
 
-    @SqsListener(
-            value = "${spring.cloud.aws.sqs.payment-success}",
-            factory = "defaultSqsListenerContainerFactory"
-    )
-    public void messageListener(List<PaymentSuccessMessage> responses) {
-        log.info("결제 성공 메시지 수신: {}", responses.size());
-        responses.forEach(response -> {
-            Payment payment = Payment.from(response);
-            paymentService.savePayment(payment);
-            orderService.approveOrder(payment.getOrderId());
-            outboxService.updateStatusToDone(payment.getOrderId());
-        });
-    }
+//    @SqsListener(
+//            value = "${spring.cloud.aws.sqs.payment-success}",
+//            factory = "defaultSqsListenerContainerFactory"
+//    )
+//    public void messageListener(List<PaymentSuccessMessage> responses) {
+//        log.info("결제 성공 메시지 수신: {}", responses.size());
+//        responses.forEach(response -> {
+//            Payment payment = Payment.from(response);
+//            paymentService.savePayment(payment);
+//            orderService.approveOrder(payment.getOrderId());
+//            outboxService.updateStatusToDone(payment.getOrderId());
+//        });
+//    }
 }

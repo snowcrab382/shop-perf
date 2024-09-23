d7c4bbcff952b321db27e0fb41a26902f50c6629
snowcrab382 <snowcrab382@naver.com>
2024-09-23 16:12:23 +0900

    refactor: 에러 로깅 AOP 전체 주석 제거

--- backend/src/main/java/perf/shop/global/aop/LoggingAspect.java
+++ backend/src/main/java/perf/shop/global/aop/LoggingAspect.java
@@ -1,23 +1,30 @@
 package perf.shop.global.aop;
 
-//@Slf4j
-//@Aspect
-//@Component
-//public class LoggingAspect {
-//
-//    @Pointcut("within(perf.shop.domain..*)")
-//    private void cut() {
-//    }
-//
-//    @Around("cut()")
-//    public Object aroundLog(ProceedingJoinPoint joinPoint) throws Throwable {
-//        try {
-//            Object result = joinPoint.proceed();
-//            return result;
-//        } catch (Exception e) {
-//            log.error("예외: {}.{}(), 클래스명 : {}", joinPoint.getSignature().getDeclaringTypeName(),
-//                    joinPoint.getSignature().getName(), e.getClass());
-//            throw e;
-//        }
-//    }
-//}
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("within(perf.shop.domain..*)")
+    private void cut() {
+    }
+
+    @Around("cut()")
+    public Object aroundLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            Object result = joinPoint.proceed();
+            return result;
+        } catch (Exception e) {
+            log.error("예외: {}.{}(), 클래스명 : {}", joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(), e.getClass());
+            throw e;
+        }
+    }
+}

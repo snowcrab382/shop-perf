49a2d7b9cd9b1f7972eaaedc1ee32b1e726c3791
snowcrab382 <snowcrab382@naver.com>
2024-09-26 18:01:39 +0900

    refactor: 불필요한 로깅 제거

--- backend/src/main/java/perf/shop/domain/payment/application/TossPaymentService.java
+++ backend/src/main/java/perf/shop/domain/payment/application/TossPaymentService.java
@@ -3,7 +3,6 @@ package perf.shop.domain.payment.application;
 
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import perf.shop.domain.payment.dto.request.PaymentApproveRequest;
@@ -11,14 +10,13 @@ import perf.shop.domain.payment.dto.response.PaymentConfirmResponse;
 import perf.shop.domain.payment.error.exception.PaymentConfirmFailedException;
 import perf.shop.infra.sqs.application.MessageSender;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TossPaymentService {
 
     private final PaymentClient paymentClient;
     private final MessageSender messageSender;
-    
+
     @Value("${spring.cloud.aws.sqs.payment-failed-queue}")
     private String PAYMENT_FAILED_QUEUE;
     @Value("${spring.cloud.aws.sqs.payment-success-queue}")
